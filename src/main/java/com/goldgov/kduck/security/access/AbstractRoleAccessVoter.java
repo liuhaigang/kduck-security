b763d2c37bb946081b207fa436fed5a5b8556862
liuhg <liuhg@mail.com>
2020-04-06 09:19:56 +0800

    调整访问授权逻辑，放弃缓存式，改为实时识别

--- src/main/java/com/goldgov/kduck/security/access/AbstractRoleAccessVoter.java
+++ src/main/java/com/goldgov/kduck/security/access/AbstractRoleAccessVoter.java
@@ -6,7 +6,6 @@ import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.authentication.AuthenticationTrustResolver;
 import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.util.AntPathMatcher;
 
@@ -44,50 +43,46 @@ public abstract class AbstractRoleAccessVoter implements RoleAccessVoter {
             String requestUri = request.getRequestURI();
             String method = request.getMethod();
 
-            Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-
-            //根据当前登录用户所拥有的角色编码查询所拥有的操作权限。
-            String[] roleCodes = new String[authorities.size()];
-            int i = 0;
-            for (GrantedAuthority authority : authorities) {
-                roleCodes[i] = authority.getAuthority();
-                i++;
-            }
-
-            List<ProtectedResource> resourceList = listResourceOperateByCode(roleCodes);
-            for (ProtectedResource resource : resourceList) {
-                if(pathMatcher.match(resource.getFullPath(),requestUri) && method.equals(resource.getOperateMethod())){
-                    result= ACCESS_GRANTED;
-                    break;
-                }
-            }
-
-            if(result == ACCESS_DENIED && permitAll(requestUri,method)){
+            if(checkAuthorize(authentication,request)){
                 result= ACCESS_GRANTED;
+            }else{
+                result= ACCESS_DENIED;
             }
+
+//            List<ProtectedResource> resourceList = listResourceOperateByCode(authentication);
+//            for (ProtectedResource resource : resourceList) {
+//                if(pathMatcher.match(resource.getFullPath(),requestUri) && method.equals(resource.getOperateMethod())){
+//                    result= ACCESS_GRANTED;
+//                    break;
+//                }
+//            }
+//
+//            if(result == ACCESS_DENIED && permitAll(requestUri,method)){
+//                result= ACCESS_GRANTED;
+//            }
         }
 
         return result;
     }
 
-    /**
-     * FIXME 处理匹配路径为"/a/b"和"/a/{b}"的情况，前者不是受保护资源，而后者是，但是后者匹配了前者。目前想到的方式是明确"/a/b"不是受保护资源
-     * @param requestUri
-     * @param method
-     * @return
-     */
-    private boolean permitAll(String requestUri,String method) {
-        //TODO 查询后加入到缓存，内存缓存即可。定时、手动刷新。
-        List<ProtectedResource> allList = listAllResourceOperate();
-        for (ProtectedResource res : allList) {
-            if(res.getOperateMethod().equals(method)){
-                if(pathMatcher.match(res.getFullPath(),requestUri)){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+//    /**
+//     * FIXME 处理匹配路径为"/a/b"和"/a/{b}"的情况，前者不是受保护资源，而后者是，但是后者匹配了前者。目前想到的方式是明确"/a/b"不是受保护资源
+//     * @param requestUri
+//     * @param method
+//     * @return
+//     */
+//    private boolean permitAll(String requestUri,String method) {
+//        //TODO 查询后加入到缓存，内存缓存即可。定时、手动刷新。
+//        List<ProtectedResource> allList = listAllResourceOperate();
+//        for (ProtectedResource res : allList) {
+//            if(res.getOperateMethod().equals(method)){
+//                if(pathMatcher.match(res.getFullPath(),requestUri)){
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+//    }
 
     private boolean permitAll(Collection<ConfigAttribute> attributes) {
         for (ConfigAttribute attribute : attributes) {
@@ -104,7 +99,9 @@ public abstract class AbstractRoleAccessVoter implements RoleAccessVoter {
         return true;
     }
 
-    public abstract List<ProtectedResource> listResourceOperateByCode(String[] roleCodes);
+    public abstract boolean checkAuthorize(Authentication authentication,HttpServletRequest request);
 
-    public abstract List<ProtectedResource> listAllResourceOperate();
+//    public abstract List<ProtectedResource> listResourceOperateByCode(Authentication authentication);
+//
+//    public abstract List<ProtectedResource> listAllResourceOperate();
 }
