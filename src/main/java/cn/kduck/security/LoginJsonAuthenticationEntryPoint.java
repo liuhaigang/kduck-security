8f01f5b38d144e449590e2d020c66d9dffe61ac6
刘海刚 <lhg_0317@163.com>
2021-02-28 20:04:29 +0800

    完善对当前用户获取接口的匹配逻辑

--- src/main/java/cn/kduck/security/LoginJsonAuthenticationEntryPoint.java
+++ src/main/java/cn/kduck/security/LoginJsonAuthenticationEntryPoint.java
@@ -1,12 +1,9 @@
 package cn.kduck.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import cn.kduck.core.utils.RequestUtils;
 import cn.kduck.core.web.json.JsonObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 import javax.servlet.ServletException;
@@ -27,7 +24,12 @@ public class LoginJsonAuthenticationEntryPoint extends LoginUrlAuthenticationEnt
 
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
-        if(request.getRequestURI().equals("/currentUser")){
+        String contextPath = request.getContextPath();
+        String requestURI = request.getRequestURI();
+        if(!"/".equals(contextPath)){
+            requestURI = requestURI.substring(contextPath.length());
+        }
+        if(requestURI.equals("/currentUser")){
             JsonObject jsonObject = new JsonObject("_ANONYMOUS_",-1,null);
             response.setContentType("application/json");
             om.writeValue(response.getOutputStream(),jsonObject);
